15ba57c54280a51780bf4245a1397d0e75510246
GeneCPChung <genecpc@gmail.com>
2017-10-06 10:43:47 -0400

    started writing code

--- src/VirtualPet.java
+++ src/VirtualPet.java
@@ -1,4 +1,45 @@
 
 public class VirtualPet {
-
+	
+	//Instance Data
+	
+	
+	
+	// Constructor
+	
+	
+	
+	//Accessor (getter) method
+	
+	
+	
+	
+	//Setter (offers change)
+	
+	
+	
+	//Method of behavior
+			
+	//tick
+	public void tick() 
+    {
+		//update everything
+		
+    }
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
 }
--- src/VirtualPetApp.java
+++ src/VirtualPetApp.java
@@ -3,6 +3,15 @@ public class VirtualPetApp {
 
 	public static void main(String[] args) {
 
+		VirtualPet pet = new pet();
+		
+		System.out.println("Tamagachi Go!");
+		
+		
+		System.out.println("Feed Me!");
+		
+		
+		
 	}
 
 }
