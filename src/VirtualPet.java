711ae658768a3571f3c62a36d9609b7a624c05e6
GeneCPChung <genecpc@gmail.com>
2017-10-06 14:42:41 -0400

    completed basics. working on stretch

--- src/VirtualPet.java
+++ src/VirtualPet.java
@@ -5,17 +5,16 @@ public class VirtualPet {
 	private String requiredAction;
 	private String petStatus;
 	private int hunger;
-	private int eating;
-	private int playing;
 	private int boredom;
 	private int needToPotty;
 	
-	
 	// Constructor
-	public VirtualPet(String requiredAction, int hunger, int boredom) {
+	public VirtualPet(String requiredAction, int hunger, int boredom, int needToPotty) {
+		
 		this.requiredAction = requiredAction;
 		this.hunger = hunger;
 		this.boredom = boredom;
+		this.needToPotty = needToPotty;
 	}
 
 	// Accessor (getter) method
@@ -23,37 +22,47 @@ public class VirtualPet {
 	public int getHunger() {
 		return hunger;
 	}
+
 	public int getBoredom() {
 		return boredom;
 	}
 
+	public int getNeedToPotty() {
+		return needToPotty;
+	}
+
 	// Setter (offers change)
-		
 
 	// Method of behavior
 
 	// tick
 	public void tick(int updatePetStatus) {
 		// update everything
-		
+		hunger += 1;
+		boredom += 1;
+		needToPotty += 1;	
+
 	}
+
 	public boolean allowAction(String enteredAction) {
 		return requiredAction.equals(enteredAction);
 	}
-	
+
 	void feeding(int eating) {
 		hunger -= eating;
+		needToPotty += eating;
+		boredom += eating;
 	}
+
 	void playing(int playing) {
 		boredom -= playing;
+		hunger += playing;
 	}
 
+	void pooping(int pooping) {
+		needToPotty -= pooping;
+		hunger += pooping;
+		boredom -= pooping;
+	}
 
-	
-	
-
-	
-	
-	
-	
 }
--- src/VirtualPetApp.java
+++ src/VirtualPetApp.java
@@ -5,14 +5,15 @@ public class VirtualPetApp {
 	public static void main(String[] args) {
 		Scanner input = new Scanner(System.in);
 		String petName = null;
-		VirtualPet pet = new VirtualPet(petName, 10, 10);
+		VirtualPet pet = new VirtualPet(petName, 49, 49, 49);
 		System.out.println("Welcome! Please give your pet a name: ");
 		String actionOpt = input.nextLine();
 		petName = actionOpt;
-		
 
 		do {
-			System.out.println("This is how " + petName + " is feeling\nHunger: " + pet.getHunger() +"\nBoredom: " + pet.getBoredom());
+			pet.tick(0);
+			System.out.println("This is how " + petName + " is feeling\nHunger: " + pet.getHunger() + "\nBoredom: "
+					+ pet.getBoredom() + "\nNeed To Potty: " + pet.getNeedToPotty());
 			System.out.println("\nWhat would you like to do?\n1: Feed\n2: Play\n3: Potty\n4: Leave\n");
 			actionOpt = input.next();
 			if (actionOpt.contentEquals("1")) {
@@ -25,17 +26,20 @@ public class VirtualPetApp {
 				}
 				if (feedingPet > 0) {
 					pet.feeding(feedingPet);
-					System.out.println(petName + "'s hunger is now: " + pet.getHunger());
+					System.out.println(petName + "'s hunger is now: " + pet.getHunger() + " and Need To Potty is " + pet.getNeedToPotty());
 				}
 			}
-			int play = 5;
+			int play = 15;
 			if (actionOpt.contentEquals("2")) {
 				pet.playing(play);
-				System.out.println(petName + "'s new boredom is now " + pet.getBoredom() + "\n");
-				
+				System.out.println(petName + "'s new boredom is now " + pet.getBoredom() + " and their hunger is " + pet.getHunger() + "\n");
 			}
-			
+			int poop = 15;
+			if (actionOpt.contentEquals("3")) {
+				pet.pooping(poop);
+				System.out.println(petName + "'s feeling much better. Her Potty is now " + pet.getNeedToPotty() + " and their Hunger is now " + pet.getHunger() + "\n");
 
+			}
 			if (actionOpt.contentEquals("4")) {
 				System.out.println("Thanks for visiting!");
 				System.exit(0);
